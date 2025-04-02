ec0dfde944b366384564f8879ecc38ea437bcaa2
IvanTusanin <ivantusanin@gmail.com>
2025-04-02 11:37:04 +0300

    ADD

---
+++ myproject/main/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+
+
+def index(request):
+    return render(request, 'main/index.html')
+
+def new(request):
+    return render(request, 'main/new.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def contact(request):
+    return render(request, 'main/contact.html')
